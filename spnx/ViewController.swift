ed8a4a0e5c89f3e2a017511114915e38f0b93ca2
Nazariy Vlizlo <vlizlo93@gmail.com>
2015-12-12 09:37:03 +0200

    UI fixed

--- spnx/ViewController.swift
+++ spnx/ViewController.swift
@@ -14,12 +14,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: false)
     }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: false)
 
-
+    }
 }
 
